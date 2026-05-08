aeb565b572a17d2ad2d39ecaf44f8dfcbf060285
krishigowthamr-bit <krishigowthamr@gmail.com>
2026-05-08 11:34:45 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
-import { Route,Routes,useNavigate } from 'react-router-dom';
+import { Route,Routes } from 'react-router-dom';
 import LogIn from './LogIn';
 
 function App() {
